0f77027dfeef8c3f671f6bffbcee77612af9bc57
FTG-003 <siar@member.fsf.org>
2025-07-04 12:18:13 +0200

    RSS feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,24 +2,29 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import YukinaConfig from '../../yukina.config';
 import type { APIContext } from 'astro';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt();
 
 export async function GET(context: APIContext) {
   const blog = await getCollection('posts');
-  const items = await Promise.all(
-    blog
-      .filter((post) => !post.data.draft)
-      .sort((a, b) => new Date(b.data.published).valueOf() - new Date(a.data.published).valueOf())
-      .map(async (post) => {
-        const { Content } = await post.render();
-        return {
-          title: post.data.title,
-          pubDate: post.data.published,
-          description: post.data.description,
-          link: `/posts/${post.slug}/`,
-          content: Content,
-        };
-      })
-  );
+  const emailLink = YukinaConfig.socialLinks.find((link) => link.link.startsWith('mailto:'));
+  const email = emailLink ? emailLink.link.replace('mailto:', '') : undefined;
+
+  const items = blog
+    .filter((post) => !post.data.draft)
+    .sort((a, b) => new Date(b.data.published).valueOf() - new Date(a.data.published).valueOf())
+    .map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.published,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+      }),
+      author: email,
+    }));
 
   return rss({
     title: YukinaConfig.title,
@@ -27,5 +32,6 @@ export async function GET(context: APIContext) {
     site: context.site!,
     items,
     customData: `<language>${YukinaConfig.locale}</language>`,
+    ...(email && { managingEditor: email, webMaster: email }),
   });
 }
