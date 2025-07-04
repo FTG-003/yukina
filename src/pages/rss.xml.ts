b205e50e24ed6e144f4037398c12d82bc8a64b20
FTG-003 <siar@member.fsf.org>
2025-07-04 12:10:53 +0200

    RSS feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,15 +1,31 @@
-import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import YukinaConfig from '../../yukina.config';
+import type { APIContext } from 'astro';
 
-export async function GET(context: { site: string }) {
-  const posts = await getCollection("posts");
+export async function GET(context: APIContext) {
+  const blog = await getCollection('posts');
+  const items = await Promise.all(
+    blog
+      .filter((post) => !post.data.draft)
+      .sort((a, b) => new Date(b.data.published).valueOf() - new Date(a.data.published).valueOf())
+      .map(async (post) => {
+        const { Content } = await post.render();
+        return {
+          title: post.data.title,
+          pubDate: post.data.published,
+          description: post.data.description,
+          link: `/posts/${post.slug}/`,
+          content: Content,
+        };
+      })
+  );
+
   return rss({
-    title: "a",
-    description: "a",
-    site: context.site,
-    items: posts.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.published,
-    })),
+    title: YukinaConfig.title,
+    description: YukinaConfig.description,
+    site: context.site!,
+    items,
+    customData: `<language>${YukinaConfig.locale}</language>`,
   });
 }
